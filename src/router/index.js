692cedca5a263ca689827cebd4c98bfaccaf8b30
Anton Orlov <orels1@orels1Mac.local>
2017-11-12 19:32:06 +0300

    router updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
+import Cogs from '@/components/Cogs';
 
 Vue.use(Router);
 
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home,
+      name: 'Cogs',
+      component: Cogs,
     },
   ],
 });
