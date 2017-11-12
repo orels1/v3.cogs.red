580f8af76c86e2844d0bc5fbc071171fa993c4a2
Anton Orlov <orels1@orels1Mac.local>
2017-11-12 19:31:44 +0300

    Home is now Cogs

---
+++ src/components/Cogs.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div(:class="$style.Cogs")
+    statsbar
+    div(:class="$style.Cogs_inner")
+      infobar
+        |These repositories are community made. We have no say over what goes into them.
+        br
+        |The author of Red and the contributors are not responsible for any damage caused by 3rd party cogs.
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Statsbar from '@/components/Statsbar';
+import Infobar from '@/components/singles/Infobar';
+
+@Component({
+  components: {
+    statsbar: Statsbar,
+    infobar: Infobar,
+  },
+})
+export default class Cogs extends Vue {
+
+}
+</script>
+
+<style lang="sass" module>
+.Cogs
+  color: #000
+
+.Cogs_inner
+  display: flex
+  flex-direction: column
+  max-width: 1200px
+  margin: 0 auto
+</style>
