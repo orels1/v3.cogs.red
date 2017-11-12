726e97fcaf4d8f040313fdd41202d2272e934a13
Anton Orlov <orels1@orels1Mac.local>
2017-11-12 21:52:27 +0300

    added Badge & Title components

---
+++ src/components/singles/Badge.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  div(:class="$style.Badge" :style={ backgroundColor: color }) {{ content }}
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  props: {
+    color: { type: String, default: '#2ecc71' },
+    text: String,
+    type: String,
+  },
+})
+export default class Badge extends Vue {
+  get content() {
+    return this.type ? this.type : this.text;
+  }
+}
+</script>
+
+<style lang="sass" module>
+.Badge
+  height: 17px
+  line-height: 17px
+  padding: 0 10px
+  font-size: 10px
+  color: #fff
+  background-color: #2ecc71
+</style>
+
+
---
+++ src/components/singles/Title.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div(:class="$style.Title")
+    slot
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Title extends Vue {
+}
+</script>
+
+<style lang="sass" module>
+.Title
+  color: #000
+  font-size: 24px
+  text-transform: uppercase
+  margin: 20px 0
+  font-weight: bold
+</style>
