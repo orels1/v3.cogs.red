0c6efd102ffbb04933178c17eb678dbc5ecfbae9
Anton Orlov <orels1@orels1Mac.local>
2017-11-12 21:52:15 +0300

    cogs list

--- src/components/Cogs.vue
+++ src/components/Cogs.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
   div(:class="$style.Cogs")
-    statsbar
+    Statsbar
     div(:class="$style.Cogs_inner")
-      infobar
+      Infobar
         |These repositories are community made. We have no say over what goes into them.
         br
         |The author of Red and the contributors are not responsible for any damage caused by 3rd party cogs.
+      CogsTitle New & Updated Cogs
+      div(:class="$style.list")
+        Cog(v-for="cog in cogs" :key="cog")
 </template>
 
 <script>
@@ -13,19 +16,27 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import Statsbar from '@/components/Statsbar';
 import Infobar from '@/components/singles/Infobar';
+import Title from '@/components/singles/Title';
+import Cog from '@/components/singles/Cog';
 
 @Component({
   components: {
-    statsbar: Statsbar,
-    infobar: Infobar,
+    Statsbar,
+    Infobar,
+    CogsTitle: Title,
+    Cog,
   },
 })
 export default class Cogs extends Vue {
-
+  cogs = [
+    'first', 'second', 'third', 'fourth', 'fith',
+  ]
 }
 </script>
 
 <style lang="sass" module>
+$mobile: 767px
+
 .Cogs
   color: #000
 
@@ -34,4 +45,12 @@ export default class Cogs extends Vue {
   flex-direction: column
   max-width: 1200px
   margin: 0 auto
+  padding: 0 20px
+
+.list
+  display: grid
+  grid-template: 130px / repeat(3, 1fr)
+  @media (max-width: $mobile)
+    grid-template: 130px / repeat(2, 1fr)
+  grid-gap: 10px 20px
 </style>
---
+++ src/components/singles/Cog.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  router-link(:class="$style.Cog" :to="{ path: links.self }" append)
+    div(:class="$style.name") {{ name }}
+    div(:class="$style.description") {{ description }}
+    div(:class="$style.tags")
+      div(:class="$style.tag" v-for="tag in tags") \#{{ tag }}
+    div(:class="$style.info")
+      div(:class="$style.author") {{ author }}
+      Badge(:class="$style.type" :type="type")
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Badge from '@/components/singles/Badge';
+
+@Component({
+  components: {
+    Badge,
+  },
+})
+export default class Cog extends Vue {
+  name = 'language';
+  description = 'Language dictionary that works with characters and alphabets'
+  tags = [
+    'translator', 'language', 'japanese',
+  ];
+  author = 'Redjumpman';
+  type = 'approved';
+  links = {
+    self: 'orels1/language',
+  };
+}
+</script>
+
+<style lang="sass" module>
+$black: #000
+$light: rgba($black, .6)
+$blueish: rgba(34,37,42,1)
+
+.Cog
+  color: $black
+  display: flex
+  flex-direction: column
+  padding: 10px
+  border: 1px solid rgba($blueish, 0)
+  transition: border 150ms ease
+  text-decoration: none
+
+  &:hover
+    border: 1px solid rgba($blueish, .6)
+
+.name
+  font-weight: bold
+
+.description
+  color: $light
+  font-size: 14px
+
+.tags
+  display: flex
+  margin: 10px 0 0 0
+  justify-content: space-between
+
+.tag
+  font-size: 12px
+  color: $light
+
+.info
+  display: flex
+  justify-content: space-between
+  margin: 5px 0 0 0
+  color: $light
+
+.author
+  font-size: 14px
+
+.type
+  align-self: center
+</style>
+
+
