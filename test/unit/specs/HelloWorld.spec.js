c0d32a68c5864b514ebd4419d7379112006a1d3c
orels1 <orel-s1@yandex.ru>
2017-12-27 14:38:23 +0300

    cleaned up tests and fixed deps

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
+import { CssModuleTestHelperMixin } from '../utils';
 
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .toEqual('Welcome to Your Vue.js App');
+Vue.use(CssModuleTestHelperMixin);
+
+describe('Test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
   });
 });
