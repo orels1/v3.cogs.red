8322e1e7dee7398a152d96afbdcc105f5d04abbb
orels1 <orel-s1@yandex.ru>
2018-01-16 02:40:08 +0300

    footer, pagination, trimming -  closes #18

--- src/components/Cogs.vue
+++ src/components/Cogs.vue
@@ -7,9 +7,13 @@
         br
         |The author of Red and the contributors are not responsible for any damage caused by 3rd party cogs.
       CogsTitle New & Updated Cogs
-      pre(v-show="!loaded") loading....
       div(:class="$style.list" v-if="loaded")
-        Cog(v-for="cog in cogs" :key="cog" :cog="cog")
+        Cog(v-for="cog in cogs.slice(0, 30 * page)" :key="cog" :cog="cog")
+      button(
+        :class="$style.show_more"
+        v-show="showMoreBtn"
+        @click="showMore"
+      ) Show More
 </template>
 
 <script>
@@ -21,6 +25,7 @@ import Infobar from '@/components/singles/Infobar';
 import Title from '@/components/singles/Title';
 import Cog from '@/components/singles/Cog';
 import c from '@/constants';
+import 'animate.css';
 
 @Component({
   components: {
@@ -33,13 +38,21 @@ import c from '@/constants';
 export default class Cogs extends Vue {
   cogs = [];
   loaded = false;
+  page = 1;
+
+  get showMoreBtn() {
+    return this.page * 30 < this.cogs.length;
+  }
+
+  showMore() {
+    this.page += 1;
+  }
 
   async created() {
     try {
       const resp = await fetch(c.COGS);
       const json = await resp.json();
-      const sorted = sortBy(json.results.list, i => i.repo.type);
-      this.cogs = sorted.slice(0, 30);
+      this.cogs = sortBy(json.results.list, i => i.repo.type);
       this.loaded = true;
     } catch (e) {
       this.error = e;
@@ -51,6 +64,10 @@ export default class Cogs extends Vue {
 <style lang="sass" module>
 $mobile: 767px
 $tiny: 440px
+$blueish: rgba(34,37,42,1)
+$hover: #43464B
+$red: rgb(236,0,26)
+$white: #fcfcfc
 
 .Cogs
   color: #000
@@ -70,4 +87,28 @@ $tiny: 440px
   @media (max-width: $tiny)
     grid-template: 130px / repeat(1, 1fr)
   grid-gap: 10px 20px
+
+.show_more
+  border: 0
+  padding: 0 40px
+  font-size: 11pt
+  font-weight: 400
+  text-transform: uppercase
+  line-height: 40px
+  color: $white
+  background: $blueish
+  align-self: center
+  margin: 15px 0
+  border-radius: 0
+  cursor: pointer
+  transform: scale(1,1)
+  transition: color 150ms ease, border-radius 150ms ease-out, transform 150ms ease
+
+  &:hover
+    background: $hover
+    border-radius: 20px
+
+  &:active
+    transform: scale(.9,.9)
+
 </style>
---
+++ src/components/singles/Footer.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div(:class="$style.Footer").
+    Want to see v3.cogs.red faster? #[a(:class="$style.link" href="https://github.com/orels1/v3.cogs.red") get involved!]
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Footer extends Vue {
+}
+</script>
+
+<style lang="sass" module>
+$white: #fcfcfc
+$blueish: rgba(34,37,42,1)
+$lred: #D5413E
+
+.Footer
+  color: $white
+  background: $blueish
+  font-size: 12px
+  text-transform: uppercase
+  margin: 40px 0 0 0
+  width: 100%
+  min-height: 100px
+  line-height: 100px
+  text-align: center
+
+.link
+  color: $lred
+  text-decoration: none
+</style>
