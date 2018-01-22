2650bb889a1b286863b58dc5e4d9d3717107abea
orels1 <orel-s1@yandex.ru>
2018-01-23 01:58:39 +0300

    added Tags & Repos fetching for statsbar (#24)

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
 const API = 'https://cogs.red/api/v1/';
 const COGS = `${API}cogs`;
 const REPOS = `${API}repos`;
+const TAGS = `${API}misc/tags/top`;
 
 export default {
   API,
   COGS,
   REPOS,
+  TAGS,
 };
