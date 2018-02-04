e6f55074d40d935d089314f143601f73bf42afc3
orels1 <orel-s1@yandex.ru>
2018-02-04 21:56:21 +0300

    Added search page #6

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,12 @@ const API = 'https://cogs.red/api/v1/';
 const COGS = `${API}cogs`;
 const REPOS = `${API}repos`;
 const TAGS = `${API}misc/tags/top`;
+const SEARCH = `${API}search/cogs`;
 
 export default {
   API,
   COGS,
   REPOS,
   TAGS,
+  SEARCH,
 };
