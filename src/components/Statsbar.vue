741e44d895100f0fc25cc47b095ff29ed1f765b8
Nick Voyloshnikov <narian@narian.ru>
2018-10-28 14:33:28 +0300

    Revert "add tags"
    This reverts commit 5a0b8225befbb95afc88c2682304036408f0ea72.

--- src/components/Statsbar.vue
+++ src/components/Statsbar.vue
@@ -23,14 +23,9 @@
             :class="$style.list_item"
             :to="'/tags/' + tag.name"
             v-if="tags.length > 0"
-            v-for="tag in tags.slice(0, 4)"
+            v-for="tag in tags.slice(0, 5)"
             :key="tag.name"
           ) {{ tag.name }}
-
-          router-link(
-            :class="$style.list_item_all"
-            :to="'/tags'"
-          ) Show all tags
 </template>
 
 <script>
@@ -168,19 +163,6 @@ $desktop: 768px
   font-size: 10pt
   color: $white
   text-decoration: none
-
-  &:hover
-    border: 1px solid rgba($white, 1)
-
-.list_item_all
-  border: 1px solid rgba($white, .3)
-  padding: 6px 10px 5px
-  transition: border 150ms ease
-  font-size: 10pt
-  color: $white
-  text-decoration: none
-  text-transform: uppercase 
-  text-align: center
 
   &:hover
     border: 1px solid rgba($white, 1)
