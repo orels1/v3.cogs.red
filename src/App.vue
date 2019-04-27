8ef76e20a3e48360309e34d674e2f651a8fd6226
orels1 <orel-s1@yandex.ru>
2019-04-28 02:43:31 +0300

    moved files around

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,53 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    .main
+      Navbar
+      router-view
+    Footer
 </template>
 
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+@Component({
+  components: {
+    Navbar,
+    Footer,
+  },
+})
+export default class App extends Vue { }
+</script>
+
+<style lang="sass" modules>
+body
+  margin: 0
+  scroll-behavior: smooth
+
+#app
+  font-family: 'Lato', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  min-height: 100vh
+  width: 100%
+  display: flex
+  flex-direction: column
+  justify-content: space-between
+</style>
+
+<style scoped>
+.main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import url("https://fonts.googleapis.com/css?family=Lato:300,400,700,900");
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VueHotkey from 'v-hotkey';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueHotkey);
 
 new Vue({
   router,
