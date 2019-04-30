4e8e7e5f18f72d120c6226e1e1fb42683c9c1194
orels1 <orel-s1@yandex.ru>
2019-04-30 03:49:01 +0300

    preparing for the new backend

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
   env = 'dev';
 }
 
-const API = env === 'prod' ? 'https://cogs.red/api/v1/' : 'http://localhost:3000/';
+const API = env === 'prod' ? 'https://api.cogs.red' : 'http://localhost:3000/';
 const COGS = `${API}cogs`;
 const REPOS = `${API}repos`;
 const TAGS = `${API}tags/top`;
