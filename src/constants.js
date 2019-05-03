1d2c7883814f9e5d77d3a569ff46e7e9afa9ecad
orels1 <orel-s1@yandex.ru>
2019-05-03 19:27:03 +0300

    fixed search url constant

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const API = process.env.VUE_APP_API;
 const COGS = `${API}cogs`;
 const REPOS = `${API}repos`;
 const TAGS = `${API}tags/top`;
-const SEARCH = `${API}search/cogs`;
+const SEARCH = `${API}search`;
 
 export default {
   API,
